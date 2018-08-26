cdb0d9f8e9e1950808ab4a6cfbd191a8ed5a1922
Ksalazar <kalfaros91@gmail.com>
2018-08-26 12:41:06 -0600

    back end is completed. need to update read me and finish front end using handelbars

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Double Cheeseburger', false);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Tofu Burger', false);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Spacy-Chicken Burger', false);
+
